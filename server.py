d5a5a079e06226083a22596ea1d883f30e4bd46c
pgalliverti-btf <pgalliverti@betterfly.com>
2026-03-25 21:47:32 -0300

    fix: servir index.html

--- server.py
+++ server.py
@@ -26,7 +26,10 @@ def subir():
         return jsonify({"ok": True, "datos": resultado})
     else:
         return jsonify({"ok": False, "mensaje": "Duplicado o requiere revisión manual"})
-    
+    @app.route("/")
+def index():
+    return open("index.html").read()
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5001))
     app.run(host="0.0.0.0", port=port)
